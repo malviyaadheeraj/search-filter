0162da93a26c87f07c937d8f24efd72b2ce1dd86
Dheeraj Malviya <76905859+malviyaadheeraj@users.noreply.github.com>
2022-03-02 18:53:33 +0530

    load more button

---
+++ src/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Posts = ({ postsToRender }) => {
+  return (
+    <ul>
+      {postsToRender.map((post, index) => (
+        <li key={index}>
+          <strong>{post.id}</strong>
+          &nbsp;{post.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default Posts;
